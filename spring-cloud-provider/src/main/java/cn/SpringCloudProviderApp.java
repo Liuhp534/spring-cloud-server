b56da4f9a962f9ed27fb450c5fcecf2533d0e5ff
Liuhp534 <617456341@qq.com>
2019-10-20 15:18:29 +0800

    加入actuator监控springboot状态信息，info可以打开

--- spring-cloud-provider/src/main/java/cn/SpringCloudProviderApp.java
+++ spring-cloud-provider/src/main/java/cn/SpringCloudProviderApp.java
@@ -2,10 +2,12 @@ package cn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient//发现服务信息
 public class SpringCloudProviderApp {
 
     public static void main(String[] args) {
--- spring-cloud-provider/src/main/java/cn/liuhp/controller/DeptController.java
+++ spring-cloud-provider/src/main/java/cn/liuhp/controller/DeptController.java
@@ -4,8 +4,13 @@ package cn.liuhp.controller;
 import cn.liuhp.entity.Dept;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/dept")
@@ -13,6 +18,8 @@ public class DeptController {
 
     private static final Logger logger = LoggerFactory.getLogger(DeptController.class);
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @RequestMapping("/searchById")
     public Dept searchById(Integer id) {
@@ -20,6 +27,18 @@ public class DeptController {
         Dept dept = new Dept();
         dept.setName("dept one");
         return dept;
+    }
+
+
+    @RequestMapping("/discovery")
+    public Object discovery() {
+        List<String> list = discoveryClient.getServices();
+        logger.info("services {}", list);
+        List<ServiceInstance> instances = discoveryClient.getInstances("spring-cloud-consumer");
+        for (ServiceInstance serviceInstance : instances) {
+            logger.info("instance {} {} {}", serviceInstance.getHost(), serviceInstance.getPort(), serviceInstance.getServiceId());
+        }
+        return discoveryClient;
     }
 
 
