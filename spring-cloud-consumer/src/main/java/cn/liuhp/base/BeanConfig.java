8b531c90a473b6fed281fc9d7d9c333a8adda92c
Liuhp534 <617456341@qq.com>
2019-10-23 22:02:02 +0800

    加入负载均衡，配置负载规则-轮训规则

--- spring-cloud-consumer/src/main/java/cn/liuhp/base/BeanConfig.java
+++ spring-cloud-consumer/src/main/java/cn/liuhp/base/BeanConfig.java
@@ -1,5 +1,8 @@
 package cn.liuhp.base;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RoundRobinRule;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -11,8 +14,15 @@ public class BeanConfig {
     * 为系统创建一个http请求工具模板
     * */
     @Bean
+    @LoadBalanced
     public RestTemplate createRestTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public IRule createRule() {
+        //其他算法，参考百度
+        return new RoundRobinRule();//轮训算法
     }
 
 }
--- spring-cloud-consumer/src/main/java/cn/liuhp/constant/UrlConstants.java
+++ spring-cloud-consumer/src/main/java/cn/liuhp/constant/UrlConstants.java
@@ -2,6 +2,8 @@ package cn.liuhp.constant;
 
 public class UrlConstants {
 
-    public static final String Dept_Service_Provider = "http://localhost:8001/dept/searchById";
+    //public static final String Dept_Service_Provider = "http://localhost:8001/dept/searchById";
+
+    public static final String Dept_Service_Provider = "http://SPRING-CLOUD-PROVIDER/dept/searchById";
 
 }
