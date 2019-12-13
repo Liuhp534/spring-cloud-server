97b03c332bc094ef72885bbb434ff434b1accad0
刘辉鹏 <liuhuipeng@huize.com>
2019-12-13 10:49:26 +0800

    加入iView-web项目

---
+++ spring-cloud-common/src/main/java/cn/liuhp/util/TestClassLoader.java
@@ -0,0 +1,13 @@
+package cn.liuhp.util;
+
+public class TestClassLoader {
+
+
+
+    public static int count = 100;
+
+    public static void main(String[] args) {
+        System.out.println(count);
+    }
+
+}
